ab0e9f41031b49a2999d9cb6ae7aa6fe8b136579
xiyueyiwan <wangteng141023@126.com>
2014-10-22 21:17:13 +0800

    Modify container class

--- Pro/anybuy/container/classes.py
+++ Pro/anybuy/container/classes.py
@@ -122,40 +122,66 @@ class Order_class:
 	OrderProfit=0
 	OrderCommodityList=[]
 	OrderCommodityNumberList=[]
-	def __init__(self,shopID,orderID):	
-   		CommodityList=[]
-		CommodityNumberList =[]
-		Cost=0
-		for o in Commodity.objects.filter( ShopID=Shop.objects.get(id=shopID)):
-			CommodityList.append(o)    
-		self.OrderCommodityList= CommodityList
-		for n in Commodity.objects.filter( ShopID=Shop.objects.get(id=shopID)):
-			CommodityNumberList.append(n.CommodityAmount)
-		self.OrderCommodityNumberList=CommodityNumberList
-		for n in Commodity.objects.filter( ShopID=Shop.objects.get(id=shopID)):
-			Cost+=n.CommodityAmount*n.SellPrice
-		self.OrderCost=Cost
-		self.OrderID=orderID
-		order=OrderList.objects.get(id = orderID)
-		self.OrderDate=order.OrderListDate
-		self.OrderState=order.OrderListState
-		self.OrderExpressID=  ""
-		# self.OrderTransfee=OrderTransfee
-		self.OrderProfit=0
+	OrderListAccount=0
+	SellID=None
+	ShopID=None
+	CustomerID=None
+	CommodityID=None
+	def __init__(self,orderID=None,shopID=None,orderListAccount=None,orderListState=None,orderListDate=None,sellID=None,customerID=None,commodityID=None):
+		if orderID!=None:
+			CommodityList=[]
+			CommodityNumberList =[]
+			Cost=0
+			for o in Commodity.objects.filter( ShopID=Shop.objects.get(id=shopID)):
+				CommodityList.append(o)    
+				self.OrderCommodityList= CommodityList
+			for n in Commodity.objects.filter( ShopID=Shop.objects.get(id=shopID)):
+				CommodityNumberList.append(n.CommodityAmount)
+				self.OrderCommodityNumberList=CommodityNumberList
+			for n in Commodity.objects.filter( ShopID=Shop.objects.get(id=shopID)):
+				Cost+=n.CommodityAmount*n.SellPrice
+			self.OrderCost=Cost
+			self.OrderID=orderID
+			order=OrderList.objects.get(id=orderID)
+			self.OrderDate=order.OrderListDate
+			self. OrderState=order.OrderListState
+			self.OrderExpressID=  ""
+			self.OrderProfit=0
+	    else:
+			self.OrderListAccount=orderListAccount
+			self.OrderState=orderListState
+			self.OrderDate=""
+			self.SellID=Seller.objects.get(id=sellID)
+			self.ShopID=Shop.objects.get(id=shopID)
+			self.CustomerID=Customer.objects.get(id=customerID)
+			self.CommodityID=Commodity.objects.get(id=commodityID)
+			self.save()
 
-	def GetCommodityList(self):
-		#OrderCommodityList=[]
-		#OrderCommodityList.append(OrderList.objects.get(id=orderID).CommodityID)
-		return self.OrderCommodityList
+	def  save(self):
+		orderList=OrderList()
+		orderList.OrderListAccount=self.OrderListAccount
+		orderList.OrderState=self.OrderState
+		orderList.OrderListDate=self.OrderDate
+		orderList.SellID=self.SellID
+		orderList.ShopID=self.ShopID
+		orderList.CustomerID=self.CustomerID
+		orderList.Commodity=self.CommodityID
+		orderList.save()
+
+
+	def GetCommodityList(self,orderID):
+		OrderCommodityList=[]
+		OrderCommodityList.append(OrderList.objects.get(id=orderID).CommodityID)
+		return  OrderCommodityList
 
 	def SetOrderState(self,orderState):
-		order=OrderList.objects.get(id = self.OrderID)
+		order=OrderList.objects.get(id=self.OrderID)
 		order.OrderListState=orderState
 		order.save()
 
 	def GetOrderProfit(self):
 		TotalCost=0
 		for commodity in self.OrderCommodityList: 
-			TotalCost+= (commodity.SoldAmount * (commodity.SellPrice-commodity.PurchasePrice))
+			TotalCost+=(commodity.SoldAmount *(commodity.SellPrice-commodity.PurchasePrice))
 		return TotalCost
 
